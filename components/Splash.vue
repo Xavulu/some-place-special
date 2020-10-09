a1ce8052e2906701277fb9fb88721b61c61688d9
Xavier Fernandez <lain@MacBook-Pro.home>
2020-10-09 07:10:56 -0400

    slightly spruced up front page, laid down the groundwork for something else :)

--- components/Splash.vue
+++ components/Splash.vue
@@ -7,12 +7,12 @@
             </div>
         <div class="inset-2 flex items-center justify-bottom">
             <div class="grid grid-cols-3 gap-10">
-                <div class="inline-block font-bold px-4 py-2 m-2"> ->  <strong class="text-light-logo dark:text-dark-blue">about[ <nuxt-link to="/about" class="text-light-red dark:text-dark-logo "> ??? </nuxt-link> ]</strong></div>
-                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">work[ <nuxt-link to="/work" class="text-light-red dark:text-dark-logo"> ??? </nuxt-link> ]</strong></div>
-                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">fun[ <nuxt-link to="/fun" class="text-light-red dark:text-dark-logo"> ??? </nuxt-link> ]</strong></div> 
-                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">github[ <a href="https://github.com/Xavulu" class="text-light-red dark:text-dark-logo">???</a> ]</strong></div> 
-                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">last.fm[ <a href="https://www.last.fm/user/natu2k" class="text-light-red dark:text-dark-logo">???</a> ]</strong></div>
-                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">.....[ <nuxt-link to="/fun/terminus" class="text-light-red dark:text-dark-logo">👼</nuxt-link> ]</strong></div>
+                <div class="inline-block font-bold px-4 py-2 m-2"> ->  <strong class="text-light-logo dark:text-dark-blue">about[ <nuxt-link to="/about" class="text-light-red dark:text-dark-logo hover:bg-light-hover dark-hover:bg-dark-dhover ">+</nuxt-link> ]</strong></div>
+                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">work[ <nuxt-link to="/work" class="text-light-red dark:text-dark-logo hover:bg-light-hover dark-hover:bg-dark-dhover">+</nuxt-link> ]</strong></div>
+                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">fun[ <nuxt-link to="/fun" class="text-light-red dark:text-dark-logo hover:bg-light-hover dark-hover:bg-dark-dhover">+</nuxt-link> ]</strong></div> 
+                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">github[ <a href="https://github.com/Xavulu" class="text-light-red dark:text-dark-logo hover:bg-light-hover dark-hover:bg-dark-dhover">+</a> ]</strong></div> 
+                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">last.fm[ <a href="https://www.last.fm/user/natu2k" class="text-light-red dark:text-dark-logo hover:bg-light-hover dark-hover:bg-dark-dhover">+</a> ]</strong></div>
+                <div class="inline-block font-bold px-4 py-2 m-2"> -> <strong class="text-light-logo dark:text-dark-blue">.....[ <nuxt-link to="/fun/terminus" class="text-light-red dark:text-dark-logo hover:bg-light-hover dark-hover:bg-dark-dhover">?</nuxt-link> ]</strong></div>
             </div>
         </div>
     </div>
